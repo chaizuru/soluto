3a6195d4fb128fc354889f71bf3b071d7cdd38fd
Pablo Barria <pablo.barria.aguilar@gmail.com>
2020-09-02 23:04:52 -0500

    agregado 2 selects de fechas y de tipo de indicador  a pagina principal

--- application/views/indicadores.php
+++ application/views/indicadores.php
@@ -4,6 +4,7 @@
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js" ></script>
 <script type="text/javascript" src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
@@ -11,9 +12,7 @@
 <body>
 
     
-<center><div id="chart-container" style="position: relative; height:40vh; width:80vw">
-        <canvas id="graphCanvas"></canvas>
-    </div></center>
+
 
     <script>
         $(document).ready(function () {
@@ -80,5 +79,39 @@
 
 <script src="https://canvasjs.com/assets/script/jquery-1.11.1.min.js"></script>
 <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+
+  
+    <div class="container">
+  <div class="row">
+    <div class="col-sm">
+    <h4>Fecha Comienzo</h4>
+    <input type="date" id="start" name="trip-start"
+       value="2018-07-22"
+       min="2018-01-01" max="2018-12-31"><br>
+       <h4>Fecha Fin</h4>
+    <input type="date" id="start" name="trip-start"
+       value="2018-07-22"
+       min="2018-01-01" max="2018-12-31"><br>
+    <h4>Tipo de Indicador</h4>
+    <select name="select">
+  <option value="value1">Value 1</option> 
+  <option value="value2" selected>Value 2</option>
+  <option value="value3">Value 3</option>
+</select>
+    <div><br><button type="button" class="btn btn-primary">Seleccionar</button>
+   
+  </div>
+    </div>
+    <div class="col-sm">
+    <div id="chart-container" style=" height:40vh; width:60vw">
+        <canvas id="graphCanvas"></canvas>
+    </div>
+    </div>
+    <div class="col-sm">
+      
+    </div>
+  </div>
+</div>
+
 </body>
 </html>
