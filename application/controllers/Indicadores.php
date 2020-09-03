3b119c8582f2e2c5608fd0ab9abf7562e8bc3257
Pablo Barria <pablo.barria.aguilar@gmail.com>
2020-09-02 19:43:08 -0500

    enconde json a array con datos filtrados

--- application/controllers/Indicadores.php
+++ application/controllers/Indicadores.php
@@ -27,10 +27,12 @@ class Indicadores extends CI_Controller {
 		$this->load->view('indicadores');
 	}
 	public function retornar_valor(){
+		$this->load->helper('array');
 		$tipo = $_GET['tipo'];
-		//$fecha= $_GET['fecha'];
+		$fechadesde= $_GET['fechadesde'];
+		$fechahasta= $_GET['fechahasta'];
 		$this->load->model('indicador_model','indicador');
-		$this->indicador->obtener_indicador($tipo,$fecha);
+		$this->indicador->obtener_indicador($tipo,$fechadesde,$fechahasta);
 	}
 	
 }
--- application/models/Indicador_model.php
+++ application/models/Indicador_model.php
@@ -1,12 +1,15 @@
 <?php
 
 class Indicador_model extends CI_Model {
-    
-    
+   
 
-    public function obtener_indicador( $tipo_indicador, $fecha){
+    
+    
+    
+    public function obtener_indicador( $tipo_indicador, $fechadesde, $fechahasta){
         $indicador_tipo_fecha='https://mindicador.cl/api/uf';
         if ( ini_get('allow_url_fopen') ) {
+            
             $json = file_get_contents($indicador_tipo_fecha);
         } else {
             //De otra forma utilizamos cURL
@@ -17,12 +20,19 @@ class Indicador_model extends CI_Model {
         }
        
         $result = json_decode($json);
-
+        $arr = array();
+        $jsonfiltrado;
         foreach ($result->serie as $uf) {
            
-        if($uf->fecha>='2020-08-30' && $uf->fecha<='2020-09-12')
+        if($uf->fecha>=$fechadesde && $uf->fecha<=$fechahasta)
             echo $uf->fecha;
+            echo "::::::::::::     ";
+            //echo $uf;
+            $arr[$uf->fecha] = $uf->valor;
+
         }
+        $jsonfiltrado=json_encode($arr);
+        echo $jsonfiltrado;
         
      
         
